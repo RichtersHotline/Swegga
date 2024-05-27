722ba4cb29e2c810faf52f4cf4a121938f96d6da
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-27 16:27:05 +0200

    added more stuff

---
+++ Scripts/code.js
@@ -0,0 +1,21 @@
+let FPrice = 150.95;
+let MPrice = 180.95;
+let FQuantity = document.getElementById("QuantityF")
+let MQuantity = document.getElementById("QuantityM")
+let TotalFe = document.getElementById("")
+
+
+function Total() {
+
+if (FQuantity.value == undefined)
+
+    MQuantity.value * MPrice
+
+else if (MQuantity.value == undefined) {
+
+    FQuantity.value * MPrice
+
+
+}
+
+}
