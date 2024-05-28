944a7de8572fc8e1d93c546da8a3df6970d34145
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-28 15:49:53 +0200

    cleaned up variables

--- Scripts/code.js
+++ Scripts/code.js
@@ -1,21 +1,64 @@
 let FPrice = 150.95;
 let MPrice = 180.95;
-let FQuantity = document.getElementById("QuantityF")
-let MQuantity = document.getElementById("QuantityM")
-let TotalFe = document.getElementById("")
 
+let TotalSum = document.getElementById("TotalAll")
+let TotalMale = document.getElementById("TotalM")
+let TotalFem = document.getElementById("TotalF") 
+// function Total() {
 
+// if (FQuantity == undefined) {
+//    console.log("Posterior")
+//      TotalSum.innerHTML = MQuantity * MPrice;
+
+// }
+// else if (MQuantity == undefined) {
+
+//     FQuantity * MPrice;
+//     TotalSum.innerHTML = MQuantity * MPrice;
+//     console.log("Rear")
+
+// }
+// else {
+
+// console.log(MQuantity += FQuantity * MPrice * FPrice);
+// console.log("Fake")
+
+// }
+
+// }
 function Total() {
+    let FQuantity = document.getElementById("QuantityF").value
+    let MQuantity = document.getElementById("QuantityM").value
+    let MaleTotal =  MQuantity * MPrice    
+    let FemTotal =  FQuantity * FPrice
+    if (FQuantity.length == 0 && MQuantity.length == 0) {
 
-if (FQuantity.value == undefined)
+console.log("empty")
+TotalSum.innerHTML = "No Quantities were selected"
 
-    MQuantity.value * MPrice
+    }
 
-else if (MQuantity.value == undefined) {
+    else if (FQuantity.length > 0 && MQuantity.length == 0) {
+     
+        TotalSum.innerHTML = FemTotal
+        TotalFem.innerHTML = FemTotal
+    }
 
-    FQuantity.value * MPrice
+    else if (MQuantity.length > 0 && FQuantity.length == 0) { 
+        
+        MaleTotal.toFixed(2)
+        TotalSum.innerHTML = MaleTotal
+        TotalMale.innerHTML = MaleTotal
 
 
-}
+    }
+
+    else if (MQuantity.length > 0 && FQuantity.length > 0) {
+        let Overall = MQuantity * MPrice + FQuantity * FPrice
+    TotalMale.innerHTML = MaleTotal
+    TotalFem.innerHTML = FemTotal
+    TotalSum.innerHTML = Overall
+
+    } 
 
 }
